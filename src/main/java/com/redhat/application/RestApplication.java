929ffb48281ee4fdcb8dae8f3ea1c97231d627ce
Naveen Malik <nmalik@redhat.com>
2014-02-25 15:22:27 -0500

    Switched over from jmxtrans to hystrix+servo

--- src/main/java/com/redhat/application/RestApplication.java
+++ src/main/java/com/redhat/application/RestApplication.java
@@ -2,9 +2,16 @@ package com.redhat.application;
 
 import com.netflix.hystrix.contrib.servopublisher.HystrixServoMetricsPublisher;
 import com.netflix.hystrix.strategy.HystrixPlugins;
+import com.netflix.servo.publish.BasicMetricFilter;
+import com.netflix.servo.publish.MetricObserver;
+import com.netflix.servo.publish.MonitorRegistryMetricPoller;
+import com.netflix.servo.publish.PollRunnable;
+import com.netflix.servo.publish.PollScheduler;
+import com.netflix.servo.publish.graphite.GraphiteMetricObserver;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
@@ -12,7 +19,37 @@ import javax.ws.rs.core.Application;
 @ApplicationPath("/rest")
 public class RestApplication extends Application {
     static {
+        // register hystrix servo metrics publisher
         HystrixPlugins.getInstance().registerMetricsPublisher(HystrixServoMetricsPublisher.getInstance());
+
+        // try to get name from openshift.  assume it's scaleable app.
+        // format: <app name>-<namespace>.<gear dns>
+        String prefix = System.getProperty("HOSTNAME)"); // default
+
+        if (System.getProperty("OPENSHIFT_APP_NAME") != null) {
+            prefix = String.format(
+                    "%s.%s.%s",
+                    System.getProperty("OPENSHIFT_APP_NAME"),
+                    System.getProperty("OPENSHIFT_NAMESPACE"),
+                    System.getProperty("OPENSHIFT_GEAR_DNS")
+            );
+        }
+
+        if (prefix == null || !prefix.isEmpty()) {
+            prefix = "naveen.was.here";
+        }
+
+        String addr = "metrics11.devlab.redhat.com:8080";
+        MetricObserver observer = new GraphiteMetricObserver(prefix, addr);
+
+        // create poller
+        PollScheduler.getInstance().start();
+
+        // create poller on observer
+        PollRunnable task = new PollRunnable(new MonitorRegistryMetricPoller(), BasicMetricFilter.MATCH_ALL, observer);
+
+        // register poller with scheduler
+        PollScheduler.getInstance().addPoller(task, 1, TimeUnit.MINUTES);
     }
 
     @Override
