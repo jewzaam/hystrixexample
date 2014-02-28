2e677d4b07d27d6d731db17d9b2fba78523664be
Naveen Malik <nmalik@redhat.com>
2014-02-28 16:44:47 -0500

    Fixed pom name etc

--- src/main/bin/swagger-properties.sh
+++ src/main/bin/swagger-properties.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-SWAGGER_BASE=`env | grep OPENSHIFT_APP_DNS | awk -F= '{print "http://" $2}'`
+SWAGGER_BASE=`env | grep OPENSHIFT_APP_DNS | awk -F= '{print "http://" $2 "/apidocs"}'`
 
 echo "SWAGGER_BASE=$SWAGGER_BASE"
