8dc8b6748d0464c86f467441141dbeb52817b993
Naveen Malik <nmalik@redhat.com>
2014-02-28 16:46:34 -0500

    Revert swagger properties change

--- src/main/bin/swagger-properties.sh
+++ src/main/bin/swagger-properties.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-SWAGGER_BASE=`env | grep OPENSHIFT_APP_DNS | awk -F= '{print "http://" $2 "/apidocs"}'`
+SWAGGER_BASE=`env | grep OPENSHIFT_APP_DNS | awk -F= '{print "http://" $2}'`
 
 echo "SWAGGER_BASE=$SWAGGER_BASE"
