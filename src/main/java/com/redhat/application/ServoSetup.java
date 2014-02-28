7faeafbdfc846130b37c3fcba8c0dc835faee2ec
Naveen Malik <nmalik@redhat.com>
2014-02-28 17:21:11 -0500

    Fixed metrics publishing for hystrix

--- src/main/java/com/redhat/application/ServoSetup.java
+++ src/main/java/com/redhat/application/ServoSetup.java
@@ -5,6 +5,7 @@ import com.netflix.hystrix.strategy.HystrixPlugins;
 import com.netflix.servo.publish.BasicMetricFilter;
 import com.netflix.servo.publish.JvmMetricPoller;
 import com.netflix.servo.publish.MetricObserver;
+import com.netflix.servo.publish.MonitorRegistryMetricPoller;
 import com.netflix.servo.publish.PollRunnable;
 import com.netflix.servo.publish.PollScheduler;
 import com.netflix.servo.publish.graphite.GraphiteMetricObserver;
@@ -43,6 +44,10 @@ public class ServoSetup {
         // start poll scheduler
         PollScheduler.getInstance().start();
 
+        // create registry on observer                                                                                                          
+        PollRunnable registeryTask = new PollRunnable(new MonitorRegistryMetricPoller(), BasicMetricFilter.MATCH_ALL, observer);
+        PollScheduler.getInstance().addPoller(registeryTask, 5, TimeUnit.SECONDS);
+        
         // create jvm poller
         PollRunnable jvmTask = new PollRunnable(new JvmMetricPoller(), BasicMetricFilter.MATCH_ALL, observer);
         PollScheduler.getInstance().addPoller(jvmTask, 5, TimeUnit.SECONDS);
